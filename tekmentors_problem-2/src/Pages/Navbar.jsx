3c2a074e8a8a160e29493f22a8c63fb72b2b6ba5
sonuprasad66 <sp.srp1999@gmail.com>
2022-12-31 09:46:25 +0530

    tekmentors_problem-2

--- tekmentors_problem-2/src/Pages/Navbar.jsx
+++ tekmentors_problem-2/src/Pages/Navbar.jsx
@@ -19,45 +19,123 @@ export const Navbar = () => {
         bg={"white"}
       >
         <Flex gap={1}>
-          <Box p={"2px 7px"} color={"white"} bg={"red"} borderRadius={2}>
+          <Box
+            p={"2px 10px"}
+            color={"white"}
+            bg={"red"}
+            borderRadius={2}
+            mt={1}
+          >
             T
           </Box>
-          <Box color={"white"} bg={"blue"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"blue"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mb={1}
+          >
             A
           </Box>
-          <Box color={"white"} bg={"black"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"black"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mt={1}
+          >
             X
           </Box>
         </Flex>
         <Flex gap={1}>
-          <Box color={"white"} bg={"red"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"red"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mt={1}
+          >
             C
           </Box>
-          <Box color={"white"} bg={"#33FFE3"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"#33FFE3"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mb={1}
+          >
             A
           </Box>
-          <Box color={"white"} bg={"black"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"black"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mt={1}
+          >
             L
           </Box>
-          <Box color={"white"} bg={"teal"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"teal"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mb={1}
+          >
             C
           </Box>
-          <Box color={"white"} bg={"green"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"green"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mt={1}
+          >
             U
           </Box>
-          <Box color={"white"} bg={"grey"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"grey"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mb={1}
+          >
             L
           </Box>
-          <Box color={"white"} bg={"#FF6433"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"#FF6433"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mt={1}
+          >
             A
           </Box>
-          <Box color={"white"} bg={"#FF33D4"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"#FF33D4"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mb={1}
+          >
             T
           </Box>
-          <Box color={"white"} bg={"blue"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"blue"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mt={1}
+          >
             O
           </Box>
-          <Box color={"white"} bg={"#FFE933"} borderRadius={2} p={"2px 7px"}>
+          <Box
+            color={"white"}
+            bg={"#FFE933"}
+            borderRadius={2}
+            p={"2px 10px"}
+            mb={1}
+          >
             R
           </Box>
         </Flex>
